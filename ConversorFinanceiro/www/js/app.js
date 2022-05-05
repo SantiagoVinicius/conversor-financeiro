964aa6a41800f1f9aa7269d77cbe1c9f00f0f980
SantiagoVinicius <73621366+SantiagoVinicius@users.noreply.github.com>
2022-05-04 21:48:10 -0300

    ajax com jason

--- ConversorFinanceiro/www/js/app.js
+++ ConversorFinanceiro/www/js/app.js
@@ -1,21 +1,19 @@
 function ConverterRealDolar(){
 
     let api = 'https://economia.awesomeapi.com.br/last/USD'
-
+    let reais = document.querySelector('#reais').value
+    let dolar = 0
+    
     $.ajax({
         type: "GET",
         url:  api,
-        success: (function(){
-            console.log("deu certo")
-        }),
-        beforeSend:(function(){
-            console.log("Processando")
+        success: (function(retorno){
+            dolar = retorno.USDBRL.ask
+            valorConvertido = reais / dolar
+            document.querySelector('#dolares').innerHTML =  valorConvertido
         })
     })
+    
 
-    let reais = document.querySelector('#reais').value
-    let dolar = 4.92
-    valorConvertido = reais / dolar
-    document.querySelector('#dolares').innerHTML =  valorConvertido
 }
 
